f0657517c04f1dd253fde631dc96641834f30ea4
e <524457102@qq.com>
2019-05-17 15:20:52 +0800

    add files

--- src/test/java/test001Test.java
+++ src/test/java/test001Test.java
@@ -14,6 +14,14 @@ public class test001Test {
     public void test2(){
         System.out.println("002");
     }
+    @Test
+    public void test3(){
+        System.out.println("003");
+    }
+    @Test
+    public void test4(){
+        System.out.println("004");
+    }
 
 
 
